05db0e2e7e1a8f948dee1e6e1d026a30e7d747cb
Leo C. Stein <leo.stein@gmail.com>
2021-03-12 14:11:40 -0600

    examples directory with auto-generating files

---
+++ examples/make-examples.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for pat in `grep -A1 "Allowed values for pattern" graphpaper.sty | tail -n1 | cut -d\{ -f2 | cut -d\} -f1 | tr , ' '`; do
+    for col in `grep -A1 "Allowed values for colorset" graphpaper.sty | tail -n1 | cut -d\{ -f2 | cut -d\} -f1 | tr , ' '`; do
+        for paper in letterpaper a4paper; do
+            for area in fullpage textarea; do
+                sed -e "s/%PATTERN%/${pat}/" \
+                    -e "s/%COLORSET%/${col}/" \
+                    -e "s/%PAPERSIZE%/${paper}/" \
+                    -e "s/%AREA%/${area}/" \
+                    template.notex > ${pat}-${col}-${paper}-${area}.tex
+            done
+        done
+    done
+done
+
